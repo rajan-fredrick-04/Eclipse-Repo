8c03ba8c1c1fa5579f8249810713ad6a239a239d
rajan-fredrick-04 <rajan.fredrick04@gmail.com>
2023-06-21 13:51:50 +0530

    Committed java files.

---
+++ src/main/java/com/mavenrepository/mavenprojectdemo/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.mavenrepository.mavenprojectdemo;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
